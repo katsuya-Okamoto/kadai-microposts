f27e35a120244067a07101d2431627da9c3e7a32
katsuya.Okamoto <sume425@eay.jp>
2018-06-11 12:26:20 +0000

    follow unfollow 2

--- resources/views/users/followings.blade.php
+++ resources/views/users/followings.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <adide class="col-xs-4">
             <div class="panel panel-default">
-                <div class "panel-heading">
+                <div class="panel-heading">
                     <h3 class="panel-title">{{ $user->name }}</h3>
                 </div>
                 <div class="panel-body">
@@ -14,7 +14,7 @@
             @include('user_follow.follow_button', ['user'=>$user])
         </adide>
         <div class="col-xs-8">
-            <ul class="nav nav-tabs nav-justfied">
+            <ul class="nav nav-tabs nav-justified">
                 <li role="presentation" class="{{ Request::is('users/' . $user->id) ? 'active' : '' }}"><a href="{{ route('users.show', ['id' => $user->id]) }}">TimeLine <span class="badge">{{ $count_microposts }}</span></a></li>
                 <li role="presentation" class="{{ Request::is('users/*/followings') ? 'active' : '' }}"><a href="{{ route('users.followings', ['id' => $user->id]) }}">Followings <span class="badge">{{ $count_followings }}</span></a></li>
                 <li role="presentation" class="{{ Request::is('users/*/followers') ? 'active' : '' }}"><a href="{{ route('users.followers', ['id' => $user->id]) }}">Followers <span class="badge">{{ $count_followers }}</span></a></li>
